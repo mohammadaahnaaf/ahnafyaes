eb2a42d24d545a5269e426067dd87fd4a8f5c6f3
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-08-08 03:00:17 +0600

    feat: :zap: new project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,30 @@
 
 const data = [
   {
+    title: "Puzzellia",
+    subtitle: 'Math Games App',
+    image: 'images/projects/puzzellia.png',
+    link: 'https://puzzellia.vercel.app/',
+    date: '2023-07-20',
+    desc:
+      'Built for Puzzellia , this a math games web app . includes '
+      + 'API, '
+      + 'realtime web socket, '
+  },
+
+  {
+    title: "RedRich",
+    subtitle: 'Hotel, Resort App',
+    image: 'images/projects/redrich.png',
+    link: 'https://red-rich.vercel.app/',
+    date: '2023-07-12',
+    desc:
+      'Built for Red Rich club , this a Hotel, Resort web app . includes '
+      + 'API, '
+      + 'realtime web socket, '
+  },
+
+  {
     title: "Ahnafia's Bazar Web App",
     subtitle: 'E-commerce App',
     image: 'images/projects/ahnafia-bazar.png',
@@ -11,6 +35,20 @@ const data = [
       'Built for Ahnafia Bazar , this a e-commerce web app . includes '
       + 'API, '
       + 'Payment Gateway, '
+      + 'realtime web socket, '
+      + 'Push Notification ',
+  },
+
+  {
+    title: 'Ahnafia Tech Web App',
+    subtitle: 'E-commerce App',
+    image: 'images/projects/ahnafia-tech.png',
+    link: 'https://ahnafia-tech.vercel.app/',
+    date: '2021-08-21',
+    desc:
+      'Built for Ahnafia Tech , this a tech related e-commerce web app . includes '
+      + 'API, '
+      + 'Stripe, '
       + 'realtime web socket, '
       + 'Push Notification ',
   },
@@ -28,46 +66,34 @@ const data = [
       + 'realtime web socket, '
       + 'Push Notification ',
   },
-  {
-    title: 'Ahnafia Tech Web App',
-    subtitle: 'E-commerce App',
-    image: 'images/projects/ahnafia-tech.png',
-    link: 'https://ahnafia-tech.vercel.app/',
-    date: '2021-08-21',
-    desc:
-      'Built for Ahnafia Tech , this a tech related e-commerce web app . includes '
-      + 'API, '
-      + 'Stripe, '
-      + 'realtime web socket, '
-      + 'Push Notification ',
-  },
-  {
-    title: 'HisabKori [Client Project]',
-    subtitle: 'Inventory management web application.',
-    image: 'images/projects/hisabkori.png',
-    link: 'https://hisabkori.vercel.app/',
-    date: '2022-02-21',
-    desc:
-      'Built for HisabKori , this a tech related e-commerce web app . includes '
-      + 'API, '
-      + 'MongoDB, '
-      + 'Next.JS, '
-      + 'MUI, ',
-  },
+  
+  // {
+  //   title: 'HisabKori [Client Project]',
+  //   subtitle: 'Inventory management web application.',
+  //   image: 'images/projects/hisabkori.png',
+  //   link: 'https://hisabkori.vercel.app/',
+  //   date: '2022-02-21',
+  //   desc:
+  //     'Built for HisabKori , this a tech related e-commerce web app . includes '
+  //     + 'API, '
+  //     + 'MongoDB, '
+  //     + 'Next.JS, '
+  //     + 'MUI, ',
+  // },
 
-  {
-    title: 'Weather App',
-    subtitle: 'This site tell you the live and real-time weather info over 600 cities',
-    link: 'https://mohammadaahnaaf.github.io/weather-app/',
-    image: '/images/projects/weather-app.png',
-    date: '2020-09-20',
-    desc:
-      'Built for Weather App , this a api based realtime weather app . includes '
-      + 'API, '
-      + 'Payment Gateway, '
-      + 'realtime web socket, '
-      + 'Push Notification ',
-  },
+  // {
+  //   title: 'Weather App',
+  //   subtitle: 'This site tell you the live and real-time weather info over 600 cities',
+  //   link: 'https://mohammadaahnaaf.github.io/weather-app/',
+  //   image: '/images/projects/weather-app.png',
+  //   date: '2020-09-20',
+  //   desc:
+  //     'Built for Weather App , this a api based realtime weather app . includes '
+  //     + 'API, '
+  //     + 'Payment Gateway, '
+  //     + 'realtime web socket, '
+  //     + 'Push Notification ',
+  // },
   // {
   //   title: 'Fotoria App',
   //   subtitle: 'Photo Gallery',
@@ -79,17 +105,18 @@ const data = [
   //     + 'React, Firebase'
   //     + 'This site is still on update...',
   // },
-  {
-    title: 'My First App',
-    subtitle: 'Live Current Date and Time',
-    link: 'https://mohammadaahnaaf.github.io/my-first-js-app/',
-    image: '/images/projects/first-app.png',
-    date: '2019-12-28',
-    desc:
-      'Launched a Realtime & Current Date and Time '
-      + 'Nothing fancy at all '
-      + 'This site uses 5 lines of simple javascript only.',
-  },
+
+  // {
+  //   title: 'My First App',
+  //   subtitle: 'Live Current Date and Time',
+  //   link: 'https://mohammadaahnaaf.github.io/my-first-js-app/',
+  //   image: '/images/projects/first-app.png',
+  //   date: '2019-12-28',
+  //   desc:
+  //     'Launched a Realtime & Current Date and Time '
+  //     + 'Nothing fancy at all '
+  //     + 'This site uses 5 lines of simple javascript only.',
+  // },
 ];
 
 export default data;

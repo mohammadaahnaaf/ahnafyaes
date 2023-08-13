22f1c8e764984fb61f52a2a3cf4517675c47695e
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-08-13 12:24:26 +0600

    artworks updated

---
+++ src/data/artworks.js
@@ -0,0 +1,30 @@
+// TODO Add a couple lines about each project
+
+const data = [
+    {
+        title: 'Calligraphy Ayat',
+        subtitle: 'Ahnafya',
+        image: 'images/artworks/ayat.png',
+        tag: ['calligraphy', 'arabic']
+    },
+    {
+        title: 'Calligraphy Name',
+        subtitle: 'Ahnafya',
+        image: 'images/artworks/name.png',
+        tag: ['calligraphy', 'arabic']
+    },
+    {
+        title: 'Pattern Morocco',
+        subtitle: 'Ahnafya',
+        image: 'images/artworks/tile-1.jpg',
+        tag: ['pattern', 'arabic']
+    },
+    {
+        title: 'Pattern Morocco 2',
+        subtitle: 'Ahnafya',
+        image: 'images/artworks/name-2.jpg',
+        tag: ['pattern', 'arabic']
+    },
+];
+
+export default data;
--- src/pages/Artworks.js
+++ src/pages/Artworks.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import Main from '../layouts/Main';
 
 // import Cell from '../components/Projects/Cell';
-// import data from '../data/projects';
+import data from '../data/artworks';
 
 const Projects = () => (
   <Main
-    title="Projects"
-    description="Learn about S A Saharukh's projects."
+    title="Artworks"
+    description="Look around Mohammed Ahnaf's Artworks."
   >
     <article className="post" id="projects">
       <header>
@@ -18,14 +18,36 @@ const Projects = () => (
           <p>A breaf selection of my Artworks </p>
         </div>
       </header>
-      {/* {data.map((project) => (
-        <Cell
-          data={project}
-          key={project.title}
-        />
-      ))} */}
+      <Arts data={data} />
     </article>
   </Main>
 );
 
 export default Projects;
+
+function Arts() {
+
+  const style = {
+    borderStyle: 'solid',
+    borderWidth: '2px',
+    borderColor: '#BB7643',
+    gridColumn: 1 / 2,
+    padding: '1rem',
+    gridColumn: 'span 1'
+  }
+  const main = {
+    display: 'grid',
+    gap: '1rem',
+    gridTemplateColumns: 'repeat(2, 1fr)'
+  }
+
+  return (
+    <div style={main}>
+      {data.map((x, index) => (
+        <div style={style} key={index}>
+          <img src={x.image} height='100%' width='100%' alt='artworks-photo' />
+        </div>
+      ))}
+    </div>
+  )
+}

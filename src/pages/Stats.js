8b2fb977e3d8b543f74616c5f5c4f43f572ab821
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2024-12-04 20:36:25 +0600

    github calender removed

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -5,13 +5,13 @@ import Main from '../layouts/Main';
 
 import Personal from '../components/Stats/Personal';
 import Site from '../components/Stats/Site';
-import GitHubCalendar from 'react-github-calendar';
+// import GitHubCalendar from 'react-github-calendar';
 
 
 const Stats = () => (
   <Main
     title="Stats"
-    description="Some statistics about Mohammed Ahnaf and mldangelo.com"
+    description="Some statistics about Mohammed Ahnaf"
   >
     <article className="post" id="stats">
       <header>
@@ -20,12 +20,13 @@ const Stats = () => (
         </div>
       </header>
       <Personal />
-      <div style={{ paddingBottom: "32px" }}>
+      {/* <div style={{ paddingBottom: "32px" }}>
         <h3><span style={{ textTransform: 'lowercase', letterSpacing: '2px', fontSize: "20px", color: 'green' }}>
           <a href="https://github.com/mohammadaahnaaf"> @me</a>
         </span> on GitHub</h3>
-        <GitHubCalendar colorScheme='light' username="mohammadaahnaaf" />
-      </div>
+        {/* <GitHubCalendar username="mohammadaahnaaf" /> */
+        /* <GitHubCalendar colorScheme='light' username="mohammadaahnaaf" /> 
+      </div> */}
       <Site />
     </article>
   </Main>
